eb4c4fc84e19dc9716cf1b07128b5ddcba3009fd
Balazs Keszei <balazs.keszei@clbr.hu>
2020-05-13 22:52:28 +0200

    update sensor find bug fix

--- app/updateSesors.py
+++ app/updateSesors.py
@@ -26,10 +26,11 @@ class UpdateSensors:
                 'group': sensor.group
             })
 
-            if None != findSensor and None != sensor and (findSensor.value * float(0.9)) > float(sensor.value) or (findSensor.value * float(1.1)) < float(sensor.value):
-                sensorOne = sensorProvider.getSensor()
-                sensorTwo = sensorProvider.getSensor()
-                sensor.value = (float(sensor.value) + float(sensorOne.value) + float(sensorTwo.value)) / 3
+            if None != findSensor and None != sensor:
+                if (findSensor.value * float(0.9)) > float(sensor.value) or (findSensor.value * float(1.1)) < float(sensor.value):
+                    sensorOne = sensorProvider.getSensor()
+                    sensorTwo = sensorProvider.getSensor()
+                    sensor.value = (float(sensor.value) + float(sensorOne.value) + float(sensorTwo.value)) / 3
 
             sensors.append(sensor)
         self.__ensureSensors(sensors)
